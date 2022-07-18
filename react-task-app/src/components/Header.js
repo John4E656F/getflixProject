9ad61a2b4fd3144592f19547ac7afc7fad674279
younes sheikhlar <sheikhlaryounes@mail.com>
2022-07-18 12:33:05 +0200

    added components

---
+++ react-task-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../components/header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
