2c7e231643d872e4d6982d116a2e7deb202a175c
John4E656F <jm49andres@gmail.com>
2022-07-21 14:40:11 +0200

    fixed index.js

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import App from './App';
+import App from './app';
 
 
 ReactDOM.render(
