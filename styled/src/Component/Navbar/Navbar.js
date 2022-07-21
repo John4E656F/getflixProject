86dee9fd0653280f01826cc3ea85ffb28b16d101
younes sheikhlar <sheikhlaryounes@mail.com>
2022-07-21 16:18:06 +0200

    Search input and its animation created

--- styled/src/Component/Navbar/Navbar.js
+++ styled/src/Component/Navbar/Navbar.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FaSearch, FaRegHeart } from 'react-icons/fa';
 import './Navbar.css'
 const Navbar = () => {
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      };
+
+
+
+
+
+
   return (
     <div className='navbar'>
         <div className='menu-left'>
@@ -14,6 +26,15 @@ const Navbar = () => {
         </div>
         <div className='menu-right'>
             <ul>
+                <li>
+                    <form className='search'onSubmit={handleSubmit}>
+                        <input type='text' placeholder='Serach' className='search_input' />
+                        <button type='submit' className='search_btn'>
+                        <i class="fa-regular fa-magnifying-glass ri-serch-2-line"></i>
+                        <FaSearch />
+                        </button>
+                    </form>
+                </li>
                 <li className='mx-5'><Link className='text-light' to='/Login'>Login</Link></li>
             </ul>
         </div>
