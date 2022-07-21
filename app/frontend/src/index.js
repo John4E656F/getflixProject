e4fc6dd8abc1970154c743ac0b9e93988b33bbf9
john4e656f <neo@kali.Neo>
2022-07-22 00:28:28 +0200

    fixed frontend server

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 
 import App from './app';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+  <BrowserRouter> 
+    <App />
+  </BrowserRouter>
+</React.StrictMode>,
   );
