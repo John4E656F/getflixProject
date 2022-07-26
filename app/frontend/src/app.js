eed702e5f3a59af3afabb93a2dfdddf86c058c11
John4E656F <jm49andres@gmail.com>
2022-07-26 17:46:06 +0200

    update structure. need fixing

--- app/frontend/src/app.js
+++ app/frontend/src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello Docker!');
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Routing from "./routing";
+
+
+// Import main sass file to apply global styles
+// import "./App.scss";
+
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routing />
+		</BrowserRouter>
+	);
+}
+
+export default App;
---
+++ app/frontend/src/pages/Home/Home.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Home.css';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+// import './CarouselHome/Carousel';
+import CarouselHome from './CarouselHome/CarouselHome';
+
+function Home(){
+    return(
+        <div className='home '>
+            <Navbar />
+            <div className='header'>
+                <div classs='container-image'>
+                    <img  className="img-responsive img-fluid max-width: 100%" src="https://www.itl.cat/pngfile/big/202-2024159_one-piece-monkey-d-luffy-straw-hat-portrait.jpg" alt="logo"/>
+                </div>
+                <div className='text-head'>
+                    <h1 className='text-light display-1'>Breath of</h1>
+                    <h1 className='text-light display-1'>Thunder</h1>
+                    <p className='text-light '>Shonen Adventure Super-powers Samourai</p>
+                    <p className='text-light '>Dive into the of Zenitsu and discover <br /> his over half and why Zenitsu's true <br /> streght awakens when he is asleep</p>
+                    <div className='container-btn'>
+                    <button className='btn btn-light mr-5 px-4'>&#128898; Play</button><button className='btn btn-light'>Mor info</button>
+                    </div>
+                </div>
+            </div>
+           <div className='carousel-1'>
+            {/* Container carousel 1 */}
+                <CarouselHome />
+            {/* Container carousel End 1 */}
+           </div>
+
+
+            <Footer />
+
+           {/* End structure */}
+        </div>
+
+        // return End
+    )
+}
+
+export default Home;
+
+
+
+
+
