a3bc46d0d17a8a7cffb96865446ea9e100284b25
JaggarYussef <jaggarYussef@gmail.com>
2022-07-26 15:07:09 +0200

    added filter by genre  function

--- app/backend/routes/filter.mjs
+++ app/backend/routes/filter.mjs
@@ -1,9 +1,18 @@
 import  mongoose  from 'mongoose'
 import {animes} from '../models/animeDB.mjs'
 
+const input_genre= 'Drama';
 
-mongoose.connect(`mongodb+srv://aniflex-admin:xelfina@aniflex-db.yqpgc8j.mongodb.net/animeDB`)
 
+(()=> {
+    mongoose.connect(`mongodb+srv://aniflex-admin:xelfina@aniflex-db.yqpgc8j.mongodb.net/animeDB`)
+    console.log('mongoosle connect');
+    var db = mongoose.connection
+    db.on('error', console.error.bind(console, 'connection error: '))
+    db.once('open', () => {
+    console.log('Jeey, connection successfull!')
+})
+})();
 // var db = mongoose.connection
 // db.on('error', console.error.bind(console, 'connection error: '))
 // db.once('open', () => {
@@ -12,7 +21,7 @@ mongoose.connect(`mongodb+srv://aniflex-admin:xelfina@aniflex-db.yqpgc8j.mongodb
 
 (async ()=> {
     try {
-        const query= await animes.find({genre:{'0':'Adventure'} })
+        const query= await animes.find({"genre":{"$in":[`${input_genre}`]}})
         console.log('this is your query' +  query);
     } catch (e) {
         console.log(e.message);
