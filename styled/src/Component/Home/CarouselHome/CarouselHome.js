e65fcd6b6ed3feefab394a5e5bd646e24759aa44
younes sheikhlar <sheikhlaryounes@mail.com>
2022-07-28 09:36:48 +0200

    modified carousel home

--- styled/src/Component/Home/CarouselHome/CarouselHome.js
+++ styled/src/Component/Home/CarouselHome/CarouselHome.js
@@ -5,7 +5,6 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import './Carousel.css';
-import './Carousel';
 // import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import Rating from '../../Rating/Rating';
