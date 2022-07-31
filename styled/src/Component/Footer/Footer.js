d6c7e77b41b783f3f4e033514fa534733ce36e1e
younes sheikhlar <sheikhlaryounes@mail.com>
2022-07-31 22:45:25 +0200

    creation footer and three DIV in container footer

--- styled/src/Component/Footer/Footer.js
+++ styled/src/Component/Footer/Footer.js
@@ -20,22 +20,9 @@ export default function BasicSpeedDial() {
 
     <div className=''>
         <div className='footer'>
-          
-        <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
-        <SpeedDial
-            ariaLabel="SpeedDial basic example"
-            sx={{ position: 'absolute', bottom: 16, right: 16 }}
-            icon={<SpeedDialIcon />}
-        >
-            {actions.map((action) => (
-            <SpeedDialAction
-                key={action.name}
-                icon={action.icon}
-                tooltipTitle={action.name}
-            />
-            ))}
-        </SpeedDial>
-        </Box>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
 
     </div>
