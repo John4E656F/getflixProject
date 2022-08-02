aa1066bd7569ca5fd09f7a2051a5d0a807a35642
younes <sheikhlaryounes@gmail.com>
2022-08-02 17:10:23 +0200

    update

--- app/frontend/src/pages/Home/home.js
+++ app/frontend/src/pages/Home/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './home.css';
+import { FiPlay }  from 'react-icons/fi'
 import Nav from '../../components/Navbar/navbar';
 // import Footer from '../../components/Footer/footer';
 import Carousel2 from '../../components/Home/carouselHome';
-function Home(){
+function home(){
     return(
         <div className='home '>
           
@@ -16,7 +17,7 @@ function Home(){
                     <p className='text-light '>Shonen Adventure Super-powers <strong>Samourai</strong></p>
                     <p className='text-light '>Dive into the of Zenitsu and discover <br /> his over half and why Zenitsu's true <br /> streght awakens when he is asleep</p>
                     <div className='container-btn'>
-                        <button className='btn btn-light mr-5 px-4'>&#128898; Play</button><button className='btn btn-light'>Mor info</button>
+                        <button className='btn btn-light mr-5 px-4 '> <FiPlay /> Play</button><button className='btn btn-light'>Moreinfo</button>
                     </div>
                 </div>
             </div>
@@ -34,4 +35,4 @@ function Home(){
     )
 }
 
-export default Home;
+export default home;
--- app/frontend/src/routing.js
+++ app/frontend/src/routing.js
@@ -7,9 +7,14 @@ import { Route, Routes } from "react-router-dom";
 // import MovieState from "./contexts/movies/movieState";
 
 // import LandingPage from "./pages/LandingPage/LandingPage";
+import Welcome from './pages/Landing/landingPage.component'
 import LoginPage from "./pages/Login/login";
 import SignupPage from "./pages//Signup/signup";
 import Home from "./pages/Home/home";
+import Anime from './pages/Anime/anime'
+import Manga from './pages/Manga/manga'
+import Kids from './pages/Kids/kids'
+
 // import Profil from "./pages/Profile/index";
 
 const Routing = () => {
