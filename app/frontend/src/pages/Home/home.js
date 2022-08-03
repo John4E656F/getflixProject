de8ee9c4fb4554f1c563ab723742f36b8b3ab092
younes <sheikhlaryounes@gmail.com>
2022-08-03 14:06:04 +0200

    change backround image home page

--- app/frontend/src/pages/Home/home.js
+++ app/frontend/src/pages/Home/home.js
@@ -15,8 +15,7 @@ function home(){
             <div className='header-home'>
                 <div className='imagHeader'></div>
                 <div className='text-head '>
-                    <h1 className='text-light display-1 ml-5'>Breath of</h1>
-                    <h1 className='text-light display-1 '>Thunder</h1>
+                    <h1 className='text-light display-1 ml-5'>mikasa ackerman</h1>
                     <p className='text-light '>Shonen Adventure Super-powers <strong>Samourai</strong></p>
                     <p className='text-light '>Dive into the of Zenitsu and discover <br /> his over half and why Zenitsu's true <br /> streght awakens when he is asleep</p>
                     <div className='container-btn'>
