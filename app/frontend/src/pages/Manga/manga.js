dbac6a1a407063159aec0562c3ec0e72281b0bd7
younes <sheikhlaryounes@gmail.com>
2022-08-03 12:32:10 +0200

    with 100% for kids page

--- app/frontend/src/pages/Manga/manga.js
+++ app/frontend/src/pages/Manga/manga.js
@@ -4,7 +4,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from '../../components/Navbar/navbar'
 import Footer from '../../components/Footer/footer';
+import { FiPlay }  from 'react-icons/fi';
 import '../../components/Manga/carouselManga'
+import './manga.css'
 export default class Responsive extends Component {
   render() {
     var settings = {
@@ -42,10 +44,31 @@ export default class Responsive extends Component {
       ]
     };
     return (
-      <div>
+      <div className="manga">
         
         <Navbar />
         {/*  */}
+        <div className='header-manga'>
+                <div className='imagHeader'></div>
+                <div className='text-head '>
+                    <h1 className='text-light display-1 ml-5'>Breath of</h1>
+                    <h1 className='text-light display-1 '>Thunder</h1>
+                    <p className='text-light '>Shonen Adventure Super-powers <strong>Samourai</strong></p>
+                    <p className='text-light '>Dive into the of Zenitsu and discover <br /> his over half and why Zenitsu's true <br /> streght awakens when he is asleep</p>
+                    <div className='container-btn'>
+                        <button className='btn btn-light mr-5 px-4 '> <FiPlay /> Play</button><button className='btn btn-light'>Moreinfo</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
 
         
         <Slider {...settings}>
