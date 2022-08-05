2c5fd9b921fb152819fddbc4b6433ff2941829eb
younes <sheikhlaryounes@gmail.com>
2022-08-05 10:31:09 +0200

    change size logo

--- app/frontend/src/components/Logo/logo.js
+++ app/frontend/src/components/Logo/logo.js
@@ -2,18 +2,22 @@ import React from 'react';
 import logo from '../../assets/images/Logo-aniflix.png'; // Tell webpack this JS file uses this image
 // The style of the Navbar section is used for the logo style
 import '../Navbar/searchNav.css';
-import { css } from '@emotion/react';
+import styled from 'styled-components';
+
+
+
+const Img = styled.img`
+
+  width: 250px;
+  margin-top: -20px;
+
+`;
+
+
 
 function Logo() {
   // Import result is the URL of your image
-  return <img  src={logo} alt="Logo" className='img-fluid  ' 
-  css={css`
-  width: 100px; height: 50px; background-color: aqua;
-  @media (max-width: 583px) {
-    width: 105px;
-    
-  }
-`} />;
+  return <Img  src={logo} alt="Logo" className='img-fluid  '/>;
 }
 
 export default Logo;
