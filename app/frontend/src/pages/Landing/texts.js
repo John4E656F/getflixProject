4d46a139a8ca610291f8c006179fd496432ea024
John4E656F <jm49andres@gmail.com>
2022-08-09 13:20:31 +0200

    add assets

--- app/frontend/src/pages/Landing/texts.js
+++ app/frontend/src/pages/Landing/texts.js
@@ -21,7 +21,7 @@ export const textualMaterial = {
 			bottomText:
 				"Stream unlimited animes, anime movie, manga and hentai on your phone, tablet, laptop, smart watch, smart fridge, smart door, smart mirror, smart smart, and TV without paying more.",
 			image:
-				"https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png",
+				"https://github.com/John4E656F/getflixProject/blob/main/app/frontend/src/assets/images/mac.jpg?raw=true",
 		},
 	],
 
