638f4a9f884bd45d85e5d2a7464c024f1e494cae
younes <sheikhlaryounes@gmail.com>
2022-08-09 18:12:47 +0200

    add style and controler for video

--- app/frontend/src/pages/Video/video.js
+++ app/frontend/src/pages/Video/video.js
@@ -16,10 +16,11 @@ const VideoPage = (props) => {
     return (
         <>
             <Navbar />
-            <div className="video-page" >
+            <div className="video-page" style={{width:'100%' , height:'100vh' , display:'flex' , justifyContent:'center' , alignItems:'center' , backgroundColor:'#302620'}}>
                 <h1 className="title">{props.title}</h1>
                 {/* Only loads the YouTube player*/}
-                <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' style={{ width:'100%', height:'auto',margin:'250px 30%' ,position:"absolute" , top:'0' , left:'0' , boxShadow:'0 0 15px gray'}} />
+                {/* <ReactPlayer autoPlay={true} style={{ width:'100%', height:'auto',margin:'250px 30%' ,position:"absolute" , top:'0' , left:'0' , boxShadow:'0 0 15px gray'}} controls className='react-player' url={this.state.url} playing={this.state.playing} /> */}
 
                 <p className="description">{props.description}</p>
                 <div className="Genres">{props.genres}</div>
